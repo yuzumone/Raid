27191691e2b129b97af6f76e73230ea02302fddf
yuzumone <yuzumone@outlook.com>
2017-07-28 10:33:05 +0900

    Add android project

---
+++ app/src/main/java/net/yuzumone/raid/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.yuzumone.raid
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
