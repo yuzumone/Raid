aaf143faf54bf8c3e98163210aa59b7c29624246
yuzumone <yuzumone@outlook.com>
2017-08-02 13:13:24 +0900

    Update raid model

--- app/src/main/kotlin/net/yuzumone/raid/model/Raid.kt
+++ app/src/main/kotlin/net/yuzumone/raid/model/Raid.kt
@@ -2,11 +2,11 @@ package net.yuzumone.raid.model
 
 class Raid {
     var place: String? = ""
-    val pokemon: String? = ""
-    val date: String? = ""
-    val player: Int = 0
+    var pokemon: String? = ""
+    var time: String? = ""
+    var player: Int = 0
 
     override fun toString(): String {
-        return "Raid{place='$place', pokemon='$pokemon', date='$date', player='$player'}"
+        return "Raid{place='$place', pokemon='$pokemon', time='$time', player='$player'}"
     }
 }
