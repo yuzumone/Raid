cdb95686ae20300b6af9236246e1d668e319d070
yuzumone <yuzumone@outlook.com>
2017-08-03 10:05:56 +0900

    Add several fileds to raid model

--- app/src/main/kotlin/net/yuzumone/raid/model/Raid.kt
+++ app/src/main/kotlin/net/yuzumone/raid/model/Raid.kt
@@ -5,8 +5,12 @@ class Raid {
     var pokemon: String? = ""
     var time: String? = ""
     var player: Int = 0
+    var address: String? = ""
+    var lat: String? = ""
+    var lng: String? = ""
 
     override fun toString(): String {
-        return "Raid{place='$place', pokemon='$pokemon', time='$time', player='$player'}"
+        return "Raid{place='$place', pokemon='$pokemon', time='$time', player='$player', " +
+                "address='$address', lat='$lat', lng='$lng'}"
     }
 }
